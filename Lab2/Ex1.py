b0a1712ba9d73d176488f08c30871b1b4da7f4e3
rnkazman <kazman@hawaii.edu>
2025-09-03 12:46:30 -1000

    Create Ex1.py

---
+++ Lab2/Ex1.py
@@ -0,0 +1,12 @@
+# Ask the user to enter a number between 1 and 100.  Square the number
+# and print the result.
+# Name: Rick Kazman
+# Date: 9/3/25
+
+print("Hi there")
+value_entered = input("Please enter an integer between 1 and 100: ")
+print("The user entered ", value_entered)
+value_as_integer = int(value_entered)
+
+value_squared = value_as_integer**2
+print("The value squared is ", value_squared)
