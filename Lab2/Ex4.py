0b7475ed983732e9ad456f2d46c2f24058dbf9c3
Rick Kazman <kazman@hawaii.edu>
2025-09-05 12:23:14 -1000

    Update Ex4.py

--- Lab2/Ex4.py
+++ Lab2/Ex4.py
@@ -3,4 +3,11 @@
 # Name: Rick Kazman
 # Date: 9/5/25
 
-value_entered = input()
+value_entered = input("Please enter a floating point number between 1 and 100: ")
+
+value_entered_float = float(value_entered)
+squared_value = value_entered_float **2
+rounded_value = round(squared_value, 2)
+
+print(f"The squared value of {value_entered_float} is {rounded_value}")
+print(f"The value without rounding is {squared_value}")
