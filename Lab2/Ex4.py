82a3f6b9098b3ab426cfbba48be77cc6483005f8
rnkazman <kazman@hawaii.edu>
2025-09-05 12:12:38 -1000

    Create Ex4.py

---
+++ Lab2/Ex4.py
@@ -0,0 +1,6 @@
+# Ask the user to enter a floating point number between 1 and 100.
+# Square the number and return the value to the user, rounded to 2 decimal places.
+# Name: Rick Kazman
+# Date: 9/5/25
+
+value_entered = input()
