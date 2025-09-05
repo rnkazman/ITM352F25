8b826b896d4ee391f2dbf78a16df5008647ceb20
Rick Kazman <kazman@hawaii.edu>
2025-09-05 12:48:11 -1000

    Update Ex6.py

--- Lab2/Ex6.py
+++ Lab2/Ex6.py
@@ -4,5 +4,6 @@
 # Date: 9/5/25
 
 weight_in_pounds = input("Please enter your weight in pounds: ")
-weight_in_kilos = float(weight_in_pounds) * 0.453592
-print(f"You weigh  {weight_in_kilos} in kilograms.")
+weight_in_kilos = float(weight_in_pounds) * 0.45359237
+weight_in_kilos_rounded = round(weight_in_kilos)
+print(f"You weigh  {weight_in_kilos_rounded} kilograms.")
