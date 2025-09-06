e8375a083d0bdc45a3565c48efb333001aaea362
rnkazman <kazman@hawaii.edu>
2025-09-06 05:23:31 -1000

    Update Ex1.CP.py

--- Lab2/Ex1.CP.py
+++ Lab2/Ex1.CP.py
@@ -1,5 +1,13 @@
-# Prompt the user to enter a whole number between 1 and 100
-num_str = input("Enter a whole number between 1 and 100: ")
-num = int(num_str)
+# Prompt the user to enter a whole number between 1 and 100, with input validation
+while True:
+	num_str = input("Enter a whole number between 1 and 100: ")
+	try:
+		num = int(num_str)
+		if 1 <= num <= 100:
+			break
+		else:
+			print("Please enter a number between 1 and 100.")
+	except ValueError:
+		print("Invalid input. Please enter a whole number.")
 square = num ** 2
 print(f"You entered {num}. The square of {num} is {square}.")
