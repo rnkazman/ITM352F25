b85c2ca065b0bf7138fe03d66cd0969963d13978
Rick Kazman <kazman@hawaii.edu>
2025-09-12 13:02:22 -1000

    Changes for Lab 3

---
+++ Lab3/UseModule.2.py
@@ -0,0 +1,18 @@
+# Test to see how the functions defined in HandyMath work.
+# Name: Rick Kazman
+# Date: Sept. 12, 2025
+
+#import HandyMath as HM
+import HandyMath
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+  
+max_number = maxx(number1, number2)
+print(f"The maximum of {number1} and {number2} is {max_number}")
+
+min_number = min(number1, number2)
+print(f"The minimum of {number1} and {number2} is {min_number}")
+
+print(max)
+print(min)
---
+++ Lab4/Ex1.py
@@ -0,0 +1,10 @@
+# String Manipulation Example
+
+first = input("Enter your first name: ")
+middle_initial = input("Enter your middle initial: ")
+last = input("Enter your last name: ")
+
+full_name = last + ", "  + first 
+print("Your full name is:", full_name)
+
+print(f"Your full name is: {first} {middle_initial}. {last}")
