093d5b2e25aa26b1dac50afd70a5842b3aaeb2a0
Rick Kazman <kazman@hawaii.edu>
2025-09-24 13:15:49 -1000

    Lab 6

---
+++ Lab6/Ex1.py
@@ -0,0 +1,6 @@
+# Initialize a tuple with different emotions
+# Name: Rick Kazman
+# Date: 9/24/2025
+
+emotions = ("happy", "sad", "fear", "surprise")
+print(emotions[0] == "happy" and len(emotions) > 3)
---
+++ Lab6/Ex1b.py
@@ -0,0 +1,9 @@
+# Initialize a tuple with different emotions
+# Name: Rick Kazman
+# Date: 9/24/2025
+
+emotions = ("happy", "sad", "fear", "surprise")
+if (len(emotions) > 3 and emotions[2] == "happy"):
+    print(True)
+else:
+    print("len is not > 3 or someone isn't happy")
