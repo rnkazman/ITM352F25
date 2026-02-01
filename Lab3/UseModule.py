502abcd369fe8ccc5fd61cf519b0e2705fe8613c
rnkazman <kazman@hawaii.edu>
2026-01-31 16:20:44 -1000

    Lab3 updates

--- Lab3/UseModule.py
+++ Lab3/UseModule.py
@@ -2,23 +2,24 @@
 # Name: Rick Kazman
 # Date: Sept. 12, 2025
 
-#import HandyMath as HM
-from HandyMath import max, min
+#import HandyMath
+import HandyMath as HM
+#from HandyMath import max, min, midpoint, exponent, square_root
 
 number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
 
-mid = HandyMath.midpoint(number1, number2)
+mid = HM.midpoint(number1, number2)
 print(f"The midpoint between {number1} and {number2} is {mid}")
 
-exp = HandyMath.exponent(number1, number2)
+exp = HM.exponent(number1, number2)
 print(f"{number1} raised to the power of {number2} is {exp}")
 
-max_number = HandyMath.max(number1, number2)
+max_number = HM.max(number1, number2)
 print(f"The maximum of {number1} and {number2} is {max_number}")
 
-min_number = HandyMath.min(number1, number2)
+min_number = HM.min(number1, number2)
 print(f"The minimum of {number1} and {number2} is {min_number}")
 
-sq_root = HandyMath.square_root(number1) 
+sq_root = HM.square_root(number1) 
 print(f"The square root of {number1} is {sq_root}")
