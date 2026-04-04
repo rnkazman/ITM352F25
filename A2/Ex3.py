dffcf51321300b28a6d77085e9465accc37d2e89
rnkazman <kazman@hawaii.edu>
2026-04-04 05:59:51 -1000

    EX3
    asdf

--- A2/Ex3.py
+++ A2/Ex3.py
@@ -10,7 +10,7 @@ import time
 
 # Temporary fix.  Don't do this in production code.
 ssl._create_default_https_context = ssl._create_unverified_context
-pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_columns', None)
 # Uncomment line below to show all rows.  Be careful with large datasets!
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', '{:.2f}'.format)
